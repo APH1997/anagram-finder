44136d060ce8a9eb41559747acaf4622592eca42
Andre <andrehristu@gmail.com>
2023-11-23 08:24:07 -0500

    python refactor skeleton

---
+++ anagram_finder.py
@@ -0,0 +1,16 @@
+class Anagrams:
+    def __init__(self, scrabble_dict_path="./scrabble_dictionary.txt"):
+        """
+        Reads local scrabble file; stores in
+        an array (REFACTOR INTO TREE)
+        """
+
+    def find_anagrams(self, word):
+        """
+        Returns a list with all anagrams
+        of the input word
+        """
+
+
+def solution_tester():
+    pass

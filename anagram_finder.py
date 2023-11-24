323ee35c1645afdc2c942d9d9864eff749e9b385
Andre <andrehristu@gmail.com>
2023-11-24 13:27:50 -0500

    converted .txt into a list

--- anagram_finder.py
+++ anagram_finder.py
@@ -2,8 +2,12 @@ class Anagrams:
     def __init__(self, scrabble_dict_path="./scrabble_dictionary.txt"):
         """
         Reads local scrabble file; stores in
-        an array (REFACTOR INTO TREE)
+        super object
         """
+        self.dict_content = []
+        with open(scrabble_dict_path, "r") as file:
+            for line in file:
+                self.dict_content.append(line.replace('\n', ''))
 
     def find_anagrams(self, word):
         """
@@ -14,3 +18,38 @@ class Anagrams:
 
 def solution_tester():
     pass
+
+test = Anagrams()
+print(test.dict_content)
+"""
+I could convert the dictionary into
+ a massive nested object
+Where each nest-level corresponds with a
+word index, and obj[a] is an object
+of all letters that come after a and still can make up valid words
+
+The goal is to cut down on
+permutations time complexity;
+This can be accomplished by disqualifying
+a given permutation of a string
+as soon as possible while we are calculating it
+{
+    a: {
+        a: {
+            b: {}
+            c:
+            d:
+            ...
+        },
+        b:
+        c:
+        d:
+        ...
+    },
+    b: {}
+    c: {}
+    d: {}
+    ...
+}
+This might speed the
+"""
